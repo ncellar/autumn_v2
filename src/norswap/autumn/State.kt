a8aa987664cb2e17a4c7ed3d13424fdecee3aea8
Nicolas "Norswap" Laurent <norswap@gmail.com>
2016-07-11 01:08:19 +0200

    reformat

--- src/norswap/autumn/State.kt
+++ src/norswap/autumn/State.kt
@@ -2,6 +2,8 @@ package norswap.autumn
 import norswap.violin.utils.*
 import norswap.violin.stream.*
 
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
 /**
  * The correct implementation of this interface, coupled with the correct use of the corresponding
  * operations in [Context], ensures that the state changes made by failed parser invocations
@@ -20,15 +22,21 @@ import norswap.violin.stream.*
  */
 interface State<Snapshot: Any, Delta: Any>
 {
+    // ---------------------------------------------------------------------------------------------
+
     /**
      * Make a snapshot of this state, which can be passed to [restore] to come back to this state.
      */
     fun snapshot(): Snapshot
 
+    // ---------------------------------------------------------------------------------------------
+
     /**
      * Passed a value returned by [snapshot] to restore the state it captured.
      */
     fun restore(snap: Snapshot)
+
+    // ---------------------------------------------------------------------------------------------
 
     /**
      * Returns a delta describing the difference between the current state and that captured by
@@ -39,10 +47,14 @@ interface State<Snapshot: Any, Delta: Any>
      */
     fun diff(snap: Snapshot): Delta
 
+    // ---------------------------------------------------------------------------------------------
+
     /**
      * Applies the supplied [delta] (a value returned by [diff]) on top of the current state.
      */
     fun merge(delta: Delta)
+
+    // ---------------------------------------------------------------------------------------------
 
     /**
      * Indicates if the current state has the same *consequences* as the state captured by
@@ -54,6 +66,8 @@ interface State<Snapshot: Any, Delta: Any>
      */
     fun equiv(pos: Int, snap: Snapshot): Boolean
 
+    // ---------------------------------------------------------------------------------------------
+
     /**
      * Returns a string representation of this state.
      *
@@ -61,6 +75,8 @@ interface State<Snapshot: Any, Delta: Any>
      */
     fun toString(ctx: Context)
         = snapshotString(snapshot(), ctx)
+
+    // ---------------------------------------------------------------------------------------------
 
     /**
      * Returns a string representation of [snap].
@@ -70,6 +86,15 @@ interface State<Snapshot: Any, Delta: Any>
     fun snapshotString(snap: Snapshot, ctx: Context): String
 }
 
+////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+
+The methods that follow are similar to those declared in [State], but allow an item of type [Any]
+to be passed, automatically casting it to the correct type.
+
+ */
+// -------------------------------------------------------------------------------------------------
+
 /**
  * Like [State.restore] but coerces [snap] to the proper type.
  */
@@ -77,11 +102,15 @@ fun <Snapshot: Any> State<Snapshot, *>.restore(snap: Any) {
     restore(snap.cast())
 }
 
+// -------------------------------------------------------------------------------------------------
+
 /**
  * Like [State.diff] but coerces [snap] to the proper type.
  */
 fun <Snapshot: Any, Delta: Any> State<Snapshot, Delta>.diff(snap: Any): Delta =
     diff(snap.cast())
+
+// -------------------------------------------------------------------------------------------------
 
 /**
  * Like [State.merge] but coerces [delta] to the proper type.
@@ -90,17 +119,23 @@ fun <Delta: Any> State<*, Delta>.merge(delta: Any) {
     merge(delta.cast())
 }
 
+// -------------------------------------------------------------------------------------------------
+
 /**
  * Like [State.equiv] but coerces [snap] to the proper type.
  */
 fun <Snapshot: Any> State<Snapshot, *>.equiv(pos: Int, snap: Any) =
     equiv(pos, snap.cast())
 
+// -------------------------------------------------------------------------------------------------
+
 /**
  * Like [State.snapshotString] but coerces [snap] to the proper type.
  */
 fun <Snapshot: Any> State<Snapshot, *>.snapshotString(snap: Any, ctx: Context) =
     snapshotString(snap.cast(), ctx)
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
 /**
  * A snapshot of the parse state at a given point in the parse.
@@ -109,7 +144,8 @@ fun <Snapshot: Any> State<Snapshot, *>.snapshotString(snap: Any, ctx: Context) =
  */
 class Snapshot (val elems: List<Any>)
 {
-    fun toString(ctx: Context): String {
+    fun toString(ctx: Context): String
+    {
         val b = StringBuilder()
         b += "snapshot {"
         ctx.states.stream().zip(elems.stream()).each {
@@ -122,8 +158,12 @@ class Snapshot (val elems: List<Any>)
         return b.toString()
     }
 
+    // ---------------------------------------------------------------------------------------------
+
     override fun toString() = ">> use Snapshot.toString(Context) instead <<"
 }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
 /**
  * The difference between two parse states (the current state at the time the delta was
@@ -132,6 +172,8 @@ class Snapshot (val elems: List<Any>)
  * This global delta is the aggregation of the deltas for each [State].
  */
 class Delta (val elems: List<Any>)
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
 /*
 
