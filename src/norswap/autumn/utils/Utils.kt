c423580014be1f1022116f2fd2b28ff821714505
Nicolas "Norswap" Laurent <norswap@gmail.com>
2016-07-20 16:52:20 +0200

    new StackTraceElement utilities

--- src/norswap/autumn/utils/Utils.kt
+++ src/norswap/autumn/utils/Utils.kt
@@ -1,4 +1,5 @@
 package norswap.autumn.utils
+import kotlin.reflect.KClass
 
 /**
  * Returns a version of this string where all tabs have been fully expanded, so that each tab
@@ -24,3 +25,34 @@ fun String.expandTabs(tabSize: Int): String {
     }   }
     return buf.toString()
 }
+
+/**
+ * Indicates if the element indicates a call within the given class and method.
+ */
+fun StackTraceElement.isMethod(klass: Class<*>, method: String)
+    =  klass.isAssignableFrom(Class.forName(this.className))
+    && method == this.methodName
+
+/**
+ * A string that describes this stack trace element, in a format recognized by IntelliJ,
+ * for which it generates *correct* clickable links (important when inlining is involved).
+ */
+fun StackTraceElement.clickableString()
+    = "at " + this
+
+/**
+ * Return a string describing the source code location of the stack trace element.
+ * Extracted from [StackTraceElement.toString].
+ */
+fun StackTraceElement.location(): String = run {
+    if (fileName == null) "(Unknown Source)"
+    else if (lineNumber >= 0) "($fileName:$lineNumber)"
+    else "($fileName)"
+}
+
+/**
+ * Indicates if the element indicates a call within the given class and method.
+ */
+fun StackTraceElement.isMethod(klass: KClass<*>, method: String)
+    = isMethod(klass.java, method)
+
